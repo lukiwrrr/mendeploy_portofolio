029ebffbfc93dae6e5cb285aacce8fe3620c5b24
lukiwrrr <luckywahyuramadhan@gmail.com>
2023-10-12 00:37:08 +0700

    First commit

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '..env')
+dotenv_path = join(dirname(__file__), '..nv')
 load_dotenv(dotenv_path)
 
 MONGODB_URI = os.environ.get('MONGODB_URI')
 DB_NAME = os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI)
-db = client(DB_NAME)
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
